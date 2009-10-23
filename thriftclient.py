e65a7f39afaaea5cb3e62a3a04bedd0d549d9314
James Clarke <clarkeje@gmail.com>
2009-10-23 17:22:58 -0500

    Added recreate() to re-establish connection

--- thriftclient.py
+++ thriftclient.py
@@ -11,35 +11,52 @@ class Client:
 
     
     """
-    def __init__(self, host, port, service, framed=True):
+    def __init__(self, host, port, service, framed=True, timeout=50000):
         """Creates a new thrift client.
 
         host - host of server.
         port - port of server.
         service - the class the server implements
         framed - should this client be framed? (for non-blocking clients)
+        timeout - timeout in ms
         """
+        self.host = host
+        self.port = port
+        self.service = service
+        self.framed = framed
+        self.timeout = timeout
+        self.create()
+
+    def create(self):
         self.transport = None
         try:
-            transport = TSocket.TSocket(host, port)
-            if framed:
+            transport = TSocket.TSocket(self.host, self.port)
+            transport.setTimeout(self.timeout)
+            if self.framed:
                 transport = TTransport.TFramedTransport(transport)
             transport = TTransport.TBufferedTransport(transport)
             protocol = TBinaryProtocol.TBinaryProtocol(transport)
-            client = service.Client(protocol)
+            client = self.service.Client(protocol)
             transport.open()
         except Thrift.TException, tx:
             print 'Exception in creating client: %s' % (tx.message)
             raise tx
         self.transport = transport
         self.client = client
+
+    def recreate(self):
+        self.close()
+        self.create()
         
     def close(self):
         """Close the socket when you are done!"""
-        self.transport.close()
+        if self.transport.isOpen():
+            self.transport.close()
 
     def open(self):
         """Open a socket."""
+        if self.transport.isOpen():
+            self.transport.close();
         try:
             self.transport.open()
         except Thrift.TException, tx:
@@ -50,4 +67,5 @@ class Client:
         if attr.startswith('_'):
             raise AttributeError("No such attribute '%s'" % attr)
         return self.client.__getattribute__(attr)
+
     
